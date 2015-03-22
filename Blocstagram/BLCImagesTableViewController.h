fa5322856e56f7a52476aafe88b2a96ccb0f46f4
edheywood <edward@waffleapp.co>
2015-03-22 21:48:39 +0000

    Table View controller implemented

---
+++ Blocstagram/BLCImagesTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  BLCImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Edward Heywood on 22/03/2015.
+//  Copyright (c) 2015 Edward Heywood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCImagesTableViewController : UITableViewController
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
